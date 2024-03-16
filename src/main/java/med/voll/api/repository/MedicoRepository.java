ac9b5e605e169f3c1a5a5d613c3b389771c0bd86
SamaraMoreira <samaramoreira000@gmail.com>
2024-03-16 14:12:43 -0300

    Implementação da verificação da situação do médico, se consta como ativo.

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -12,8 +12,8 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
-    @Query(""" 
 
+    @Query("""
            select m from Medico m 
            where m.ativo = true
            and 
@@ -23,6 +23,13 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
                 select c.medico.id from Consulta c where c.data = :data
            )
            order by rand()
-           limit 1 """)
+           limit 1 
+            """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+
+    @Query("""
+            select m.ativo from Medico m where m.id = :id
+            """)
+    Boolean findByAtivoId(Long idMedico);
 }
