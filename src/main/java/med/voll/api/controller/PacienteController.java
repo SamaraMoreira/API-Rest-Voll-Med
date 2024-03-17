89242f8da1f2d6e3e58c04fbaaa52a5703681085
SamaraMoreira <samaramoreira000@gmail.com>
2024-03-16 23:26:43 -0300

    Realizando a autorização do Swagger-UI, para documentação da API.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.*;
 import med.voll.api.repository.PacienteRepository;
@@ -15,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @Transactional
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
